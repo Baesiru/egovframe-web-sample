d57533a72fdd217628a5b8053d69044d33ce549d
배준환 <baejh@seowoninfo.com>
2026-06-30 16:39:30 +0900

    빌드 가능하도록 예외 수정

--- src/main/java/egovframework/example/sample/web/EgovSampleController.java
+++ src/main/java/egovframework/example/sample/web/EgovSampleController.java
@@ -81,7 +81,9 @@ public class EgovSampleController {
 	public String selectSampleList(@ModelAttribute("searchVO") SampleDefaultVO searchVO, ModelMap model)
 			throws Exception {
 
-		throw new RuntimeException();
+		if (true) {
+			throw new RuntimeException();
+		}
 		/** EgovPropertyService.sample */
 		searchVO.setPageUnit(propertiesService.getInt("pageUnit"));
 		searchVO.setPageSize(propertiesService.getInt("pageSize"));
